b536235892c88e016dc01d66a266265547e0b07b
Ero Osamuyi <eroosamuyi11451@gmail.com>
2019-09-11 08:30:28 -0700

    feature added: bcrypt added to validation

--- login/login.ts
+++ login/login.ts
@@ -1,33 +1,31 @@
+import { logger } from "../logger";
+
 // import { logger } from "../logger";
 
-import { SHA256 } from "crypto-js"
+// import { SHA256 } from "crypto-js"
 
 const express = require('express');
 const router = express.Router();
 const userModel = require("../db/userModels");
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
 router.post("/" , async (req,res, next)=>{
     const userName = req.body.userName ;
     const password  = req.body.password ;
-    const hashedPassword = SHA256(password).toString()
-    const email = req.body.email;
-    await userModel.findOne({ where: {userName:userName, password:hashedPassword,email:email }})
-    .then((user) => {
-    if(!user){
-        res.send("user was not found please register")
-    }else{
-        jwt.sign({user}, "muyiistheboss", (err , token)=>{
-            res.json({token})
-        })
-    }
-   
-}).catch(err =>{
-        console.log(err)
-    })
-    
-   next()
+    const email = req.body.email; 
+
+    const user = await userModel.findOne({ where: {userName} })
+      .catch(error => {
+        logger.error(error)  
+        res.send(`${error.message}`)
+      })
+      const match = bcrypt.compare(password, user.password);
+      if(match) {
+          const token = jwt.sign({ user}, 'muyi');
+          res.send(token)
+       }
+  
 })
             
-
 export const loginUser = router;
--- login/regUser.ts
+++ login/regUser.ts
@@ -1,16 +1,21 @@
 import { logger } from "../logger";
-import { SHA256 } from "crypto-js"
+// import { SHA256 } from "crypto-js"
 const express = require('express')
 const router = express.Router()
 const userModel = require("../db/userModels");
-
-
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken")
 router.post("/", async(req, res, next)=>{
           
     const userName = req.body.userName ;
     const password  = req.body.password ;
-    const hashedPassword = SHA256(password).toString()
     const email = req.body.email;
+
+    const hashedPassword = bcrypt.genSalt(10, (err, salt)=>{
+      bcrypt.hash(password,salt, (err,passhash)=>{
+        return passhash
+      })
+    })
 
     await userModel.create({
           userName: userName, 
@@ -21,6 +26,7 @@ router.post("/", async(req, res, next)=>{
                 res.send(`${error.message}`)
               })
     .then((user)=>{
+                  
                  res.send(` user with the userName of ${user.userName} was created`)
                }
               )
